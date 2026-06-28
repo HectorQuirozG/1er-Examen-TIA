1e2719461355d47a58569cbbf90e47b74dadea02
HectorQuirozG <hector.quirozg@uanl.edu.mx>
2026-06-27 20:00:57 -0600

    Add files via upload

---
+++ examtool.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import json
+
+anexoa = pd.read_csv("AnexoA.csv")
+incidentes_nuevos = pd.read_csv("incidents_master-selected-columns.csv")
+
+def limpiar(df):
+    Q1 = df["Registros"].quantile(0.25)
+    Q3 = df["Registros"].quantile(0.75)
+    IQR = Q3 - Q1
+    limite_inferior = Q1 - 1.5 * IQR
+    limite_superior = Q3 + 1.5 * IQR
+    df_resultado = df[(df["Registros"] >= limite_inferior) & (df["Registros"] <= limite_superior)]
+
+    Q1 = df_resultado["Dias_Deteccion"].quantile(0.25)
+    Q3 = df_resultado["Dias_Deteccion"].quantile(0.75)
+    IQR = Q3 - Q1
+    limite_inferior = Q1 - 1.5 * IQR
+    limite_superior = Q3 + 1.5 * IQR
+    df_resultado = df_resultado[(df_resultado["Dias_Deteccion"] >= limite_inferior) & (df_resultado["Dias_Deteccion"] <= limite_superior)]
+    return df_resultado
+
+def formato_incidentes(df):
+    df["fecha_inicio"] = pd.to_datetime(df["incident_date"], format="%Y-%m-%d")
+    df["fecha_fin"] = pd.to_datetime(df["discovery_date"], format="%Y-%m-%d")
+    df["dias_deteccion"] = (df["fecha_fin"] - df["fecha_inicio"]).dt.days
+
+    with open('sectores.json', 'r', encoding='utf-8') as f:
+        sectores = json.load(f)
+        df['industry_primary'] = df['industry_primary'].map(sectores)
+    df = df.drop(columns=['incident_date', 'discovery_date', "fecha_inicio", "fecha_fin"])
+    df['attack_vector_primary'] = df['attack_vector_primary'].str.capitalize()
+    df = df[['industry_primary','attack_vector_primary','dias_deteccion','data_compromised_records']]
+    df.columns = ['Sector', 'Tipo', 'Dias_Deteccion', 'Registros']
+    return df
+    
+def combinar(df1, df2):
+    df_lista = [df1, df2]
+    df_combinado = pd.concat(df_lista, ignore_index=True)
+    return df_combinado
+
+incidentes_nuevos = formato_incidentes(incidentes_nuevos)
+
+df_combinado = combinar(anexoa, incidentes_nuevos)
+df_combinado.to_csv('dataset_raw.csv', index=False)
+df_combinado = limpiar(df_combinado)
+df_combinado.to_csv('dataset.csv', index=False)
